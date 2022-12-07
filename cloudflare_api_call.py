cda7e0269c731c432c0ef6b88d87ac83eb30c328
Sunny <shebasarah91@gmail.com>
2022-12-08 02:10:16 +1300

    2ndcommit

--- cloudflare_api_call.py
+++ cloudflare_api_call.py
@@ -1,47 +1,63 @@
+"""Cloudflare API call"""
+
 import requests
-import json
- 
-headers = {
-    'X-Auth-Email': 'helloshebasarah@gmail.com',
-    'X-Auth-Key': '2ed5a5383794be1a1ec92ccf6aaffae2c476b',
-    'Content-Type': 'application/json'
-    
-    
-}
-data={
-   'name':'readonly-token',
-   'policy':{
-    "id": "f267e341f3dd4697bd3b9f71dd96247f",
-    "effect": "allow",
-   }
-   
-} 
+from random_token_generator import RandomTokenGenerator
+from get_api_key import GetAPIKey
 
-response = requests.request(
-    'PUT',
-    'https://api.cloudflare.com/client/v4/user/tokens/54aad0c5b4e9d961a59c58d2fc86260a/value',
-     headers=headers,
-    
-    
-)
+CF_API_KEY = "Vhd7lE-isSY5QobG3k5eVCHYy87mjn9G0Kv19lTK"
+CF_ZONE_ID = "72bab892f6318efaa9451b6fa18b9a26"
+CF_RULE_ID = "b3ac5edd385a47f2827bd62d9dd923d0"
 
-if response and response.status_code == 200:
-    print('Request was successful')
-    
 
-    data=response.json()
-    print(data)
-    print(data["result"])
-    
+class TokenRefresh:
+    """Token refresh class"""
 
-# resp = requests.request(
-#      'GET',
-#      'https://api.cloudflare.com/client/v4/user/tokens',
-#      headers=headers   
-#  )
+    # roll cloudflare token secret
+    def roll_secret(self):
+        """Generate token"""
+        random_token_generator = RandomTokenGenerator()
+        token = random_token_generator.generate_random_token()
+        """Get key"""
+        # key = GetAPIKey.get_secret()
+        # print(key)
+        """Modify Token for Http Request Header"""
+        response = requests.put(
+            f"https://api.cloudflare.com/client/v4/zones/{CF_ZONE_ID}/rulesets/phases/http_request_late_transform/entrypoint",
+            headers={
+                "Authorization": f"Bearer {CF_API_KEY}",
+                "Content-Type": "application/json",
+            },
+            json={
+                "rules": [
+                    {
+                        "id": CF_RULE_ID,
+                        "description": "X-WAF-SECRET",
+                        "expression": '(http.host ne "1")',
+                        "action": "rewrite",
+                        "action_parameters": {
+                            "headers": {
+                                "X-WAF-SECRET": {
+                                    "operation": "set",
+                                    "value": token,
+                                },
+                            },
+                        },
+                        "enabled": True,
+                    },
+                ],
+            },
+            timeout=10,
+        )
+        # print(response.status_code)
+        # print(response.json())
+        # result = None
 
- #print(resp.status_code)
- 
- 
-# print(resp.json())
+        if response:
+            data = response.json()
+            print(response.status_code)
+            # print(data)
 
+            if response.status_code == 200:
+                result = data
+
+        return result
