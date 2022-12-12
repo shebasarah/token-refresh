f3d1014e58770a06125de6e68d1c5f792c9542a8
Sunny <shebasarah91@gmail.com>
2022-12-13 09:43:05 +1300

    13-Dec

--- random_token_generator.py
+++ random_token_generator.py
@@ -1,10 +1,28 @@
 import random
+import boto3
+from botocore.exceptions import ClientError
 import string
+import os
 
 """Random token generator class"""
 
 
 class RandomTokenGenerator:
     def generate_random_token(self):
-        # return "".join(random.choice(string.hexdigits) for _ in range(50))
-        return "".join(random.choice(string.hexdigits) for _ in range(30))
+
+        region_name = "ap-southeast-2"
+        exclude_characters = (
+            os.environ["EXCLUDE_CHARACTERS"]
+            if "EXCLUDE_CHARACTERS" in os.environ
+            else "/@\"'\\"
+        )
+
+        # return "".join(random.choice(string.hexdigits) for _ in range(30))
+        """ Create a Secrets Manager client"""
+        session = boto3.session.Session()
+        client = session.client(service_name="secretsmanager", region_name=region_name)
+        try:
+            response = client.get_random_password(ExcludeCharacters=exclude_characters)
+            return response["RandomPassword"]
+        except ClientError as e:
+            raise e
